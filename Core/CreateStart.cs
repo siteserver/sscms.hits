d763572a978ed8ff06c8594fd20fed640f2acd29
timlai <timlai99@outlook.com>
2021-01-21 16:45:32 +0800

    bugfix

--- Core/CreateStart.cs
+++ Core/CreateStart.cs
@@ -1,17 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SSCMS.Configuration;
 using SSCMS.Enums;
 using SSCMS.Parse;
 using SSCMS.Plugins;
+using SSCMS.Repositories;
+using SSCMS.Services;
 
 namespace SSCMS.Hits.Core
 {
-    public class CreateStart : IPluginCreateStart
+    public class CreateStart : IPluginCreateStartAsync
     {
-        public void Parse(IParseContext context)
+        private readonly IPathManager _pathManager;
+        private readonly ISiteRepository _siteRepository;
+
+        public CreateStart(IPathManager pathManager, ISiteRepository siteRepository)
+        {
+            _pathManager = pathManager;
+            _siteRepository = siteRepository;
+        }
+
+        public async Task ParseAsync(IParseContext context)
         {
             if (context.TemplateType != TemplateType.ContentTemplate || context.ContentId <= 0) return;
 
-            var apiUrl = $"/api/hits/{context.SiteId}/{context.ChannelId}/{context.ContentId}";
+            var site = await _siteRepository.GetAsync(context.SiteId);
+
+            var apiUrl = _pathManager.GetApiHostUrl(site, Constants.ApiPrefix,
+                $"/hits/{context.SiteId}/{context.ChannelId}/{context.ContentId}");
+
             context.FootCodes.TryAdd(HitsManager.PluginId, $@"<script src=""{apiUrl}"" type=""text/javascript""></script>");
         }
     }
